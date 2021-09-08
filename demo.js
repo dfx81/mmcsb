8ee1c1b3b3aeee9dfad4358a2eef07ae1e9dd1b5
dfx81 <danialfitrighazali@gmail.com>
2021-09-09 01:28:04 +0800

    3D effect added

--- demo.js
+++ demo.js
@@ -1,18 +1,15 @@
 let c = document.getElementById("canvas");
-let content = document.getElementById("content");
-let header = document.getElementById("header");
-let footer = document.getElementById("footer");
+let wrapper = document.getElementById("wrapper");
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 let ctx = c.getContext("2d");
-
 let curPts = -100;
 let lastPts = -100;
 
 let gridSize = c.width > c.height ? 7 : 5;
 gridSize = c.width / gridSize;
 
-content.addEventListener("mousemove", (evt) => {
+wrapper.addEventListener("mousemove", (evt) => {
   clear();
   x = evt.clientX || evt.pageX;
   y = evt.clientY || evt.pageY;
@@ -24,33 +21,17 @@ content.addEventListener("mousemove", (evt) => {
 
   ctx.fillStyle = "#ffffff30";
   ctx.fillRect(gridpts.x + 2, gridpts.y + 2, gridSize - 4, gridSize - 4);
+
+  let ang = x / window.innerWidth - 0.5;
+  ang = ang * 5;
+  wrapper.style.transform = "rotate3d(0, 1, 0, " + ang + "deg)";
 });
-header.addEventListener("mousemove", (evt) => {
+
+wrapper.addEventListener("mouseleave", (evt) => {
+  wrapper.style.transform = "none";
   clear();
-  x = evt.clientX || evt.pageX;
-  y = evt.clientY || evt.pageY;
-
-  gridpts = grid(x, y)
-
-  lastPts = curPts;
-  curPts = gridpts;
-
-  ctx.fillStyle = "#ffffff30";
-  ctx.fillRect(gridpts.x + 2, gridpts.y + 2, gridSize - 4, gridSize - 4);
 });
-footer.addEventListener("mousemove", (evt) => {
-  clear();
-  x = evt.clientX || evt.pageX;
-  y = evt.clientY || evt.pageY;
 
-  gridpts = grid(x, y)
-
-  lastPts = curPts;
-  curPts = gridpts;
-
-  ctx.fillStyle = "#ffffff30";
-  ctx.fillRect(gridpts.x + 2, gridpts.y + 2, gridSize - 4, gridSize - 4);
-});
 
 function clear() {
   ctx.clearRect(0, 0, c.width, c.height);
@@ -63,8 +44,10 @@ let grid = (x, y) => {
   return {x: gridx, y: gridy};
 }
 
+let ang = 0
+
 let loop = () => {
-  window.requestAnimationFrame(loop);
+  //todo
 };
 
-window.requestAnimationFrame(loop);
+//window.requestAnimationFrame(loop);
